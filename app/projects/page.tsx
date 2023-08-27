804ba8c213656f9ae08d14039dbe2d587f11d0c3
unknown <iverfinne@gmail.com>
2023-08-27 15:36:02 +0200

    te

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,7 @@
-// File: ./app/projects/page.tsx
-
 import Link from "next/link";
 import React from "react";
 import { allProjects } from "contentlayer/generated";
+import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 import { Article } from "./article";
 import { Redis } from "@upstash/redis";
@@ -10,34 +9,34 @@ import { Eye } from "lucide-react";
 
 const redis = Redis.fromEnv();
 
-export const revalidate = 60;
 
+export const revalidate = 60;
 export default async function ProjectsPage() {
-  const views = (
-    await redis.mget<number[]>(
-      ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":"))
-    )
-  ).reduce((acc, v, i) => {
-    acc[allProjects[i].slug] = v ?? 0;
-    return acc;
-  }, {} as Record<string, number>);
+	const views = (
+		await redis.mget<number[]>(
+			...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":")),
+		)
+	).reduce((acc, v, i) => {
+		acc[allProjects[i].slug] = v ?? 0;
+		return acc;
+	}, {} as Record<string, number>);
 
   const featured = allProjects.find((project) => project.slug === "emberlight")!;
   const top2 = allProjects.find((project) => project.slug === "storygen")!;
   const top3 = allProjects.find((project) => project.slug === "ygdrasyl")!;
-  const sorted = allProjects
-    .filter((p) => p.published)
-    .filter(
-      (project) =>
-        project.slug !== featured.slug &&
-        project.slug !== top2.slug &&
-        project.slug !== top3.slug
-    )
-    .sort(
-      (a, b) =>
-        new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
-    );
+	const sorted = allProjects
+		.filter((p) => p.published)
+		.filter(
+			(project) =>
+				project.slug !== featured.slug &&
+				project.slug !== top2.slug &&
+				project.slug !== top3.slug,
+		)
+		.sort(
+			(a, b) =>
+				new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+				new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
+		);
 
   return (
     <div className="relative pb-16">
