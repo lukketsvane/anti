dc4a715a8e9af9e4618fa3335097844b2e67ef7c
unknown <iverfinne@gmail.com>
2023-08-27 02:37:27 +0200

    te

--- app/page.tsx
+++ app/page.tsx
@@ -29,13 +29,13 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				finnedesign
+				finne design
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-				Hi, my name is Iver Finne, I'm a designer and software developer at{" "}
+				Hi, my name is Iver Finne, I'm a designer and software developer. Follow me at{" "}
 					<Link
 						target="_blank"
 						href="https://github.com/lukketsvane/"
@@ -45,7 +45,7 @@ export default function Home() {
 					</Link>
 
 					<br />
-					and working on{" "}
+					or contact me on{" "}
 					<Link
 						target="_blank"
 						href="https://www.linkedin.com/in/iverfinne"
