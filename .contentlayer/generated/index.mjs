e6dcda1f5902d933c0e98a01eb16a796cb754f59
unknown <iverfinne@gmail.com>
2023-08-29 18:50:08 +0200

    halfway updated contact page

--- .contentlayer/generated/index.mjs
+++ .contentlayer/generated/index.mjs
@@ -4,10 +4,10 @@ export { isType } from 'contentlayer/client'
 
 // NOTE During development Contentlayer imports from `.mjs` files to improve HMR speeds.
 // During (production) builds Contentlayer it imports from `.json` files to improve build performance.
-import allPages from './Page/_index.json' assert { type: 'json' }
-import allProjects from './Project/_index.json' assert { type: 'json' }
-import allGalleryImages from './GalleryImage/_index.json' assert { type: 'json' }
-import allGalleryProjects from './GalleryProject/_index.json' assert { type: 'json' }
+import { allPages } from './Page/_index.mjs'
+import { allProjects } from './Project/_index.mjs'
+import { allGalleryImages } from './GalleryImage/_index.mjs'
+import { allGalleryProjects } from './GalleryProject/_index.mjs'
 
 export { allPages, allProjects, allGalleryImages, allGalleryProjects }
 
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,10 +2,14 @@
 
 "use client";
 import React, { useEffect, useState } from "react";
-import { Github, Mail, Twitter, Moon, Sun } from "lucide-react";
+import { Github, Mail, Twitter, Sun, Moon } from "lucide-react";
 import Link from "next/link";
-import { Navigation } from "../components/nav";
-import { Card } from "../components/card";
+
+const navigation = [
+  { name: 'Projects', href: '/projects' },
+  { name: 'Gallery', href: '/gallery' },
+  { name: 'Contact', href: '/contact' },
+];
 
 const socials = [
   {
@@ -44,9 +48,27 @@ export default function ContactPage() {
   };
 
   return (
-    <div className={`relative bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 ${darkMode ? "text-white" : "text-black"}`}>
-      <Navigation darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
-      <div className="container px-6 pt-16 mx-auto max-w-7xl lg:px-8 md:space-y-16 md:pt-20 lg:pt-24">
+    <div className={`relative min-h-screen ${darkMode ? 'bg-gradient-to-tl from-black via-zinc-600/20 to-black' : 'bg-white'}`}>
+      <header>
+        <div className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b ${darkMode ? 'bg-zinc-900/0 border-transparent' : 'bg-zinc-900/500 border-zinc-800'}`}>
+          <div className="container flex items-center justify-between p-6 mx-auto">
+            <Link href="/">
+              Home
+            </Link>
+            <div className={`flex justify-end gap-8 ${darkMode ? 'text-white' : 'text-black'}`}>
+              <button onClick={toggleDarkMode}>
+                {darkMode ? <Sun size={20} /> : <Moon size={20} />}
+              </button>
+              {navigation.map((item) => (
+                <Link key={item.href} href={item.href}>
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </header>
+      <div className="container px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <h2 className="mb-4 text-3xl font-bold tracking-tight sm:text-4xl">
           Contact
         </h2>
@@ -56,29 +78,21 @@ export default function ContactPage() {
         <div className="w-full h-px bg-zinc-800" />
         <div className="grid grid-cols-1 gap-10 mx-auto mt-8 sm:grid-cols-3 lg:grid-cols-3">
           {socials.map((s) => (
-            <Card key={s.href}>
-              <Link
-                href={s.href}
-                target="_blank"
-                className={`p-14 relative flex flex-col items-center duration-700 group md:gap-8 md:py-24 lg:pb-48  md:p-16 ${darkMode ? "text-white" : "text-black"}`}
-              >
-                <span
-                  className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-                  aria-hidden="true"
-                />
-                <span className={`relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange`}>
+            <div key={s.href} className="p-14 relative flex flex-col items-center duration-700 group md:gap-8 md:py-24 lg:pb-48 md:p-16">
+              <Link href={s.href} target="_blank">
+                <div className={`relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange`}>
                   {s.icon}
-                </span>{" "}
-                <div className="z-10 flex flex-col items-center">
-                  <span className={`text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display`}>
-                    {s.handle}
-                  </span>
-                  <span className={`mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200`}>
-                    {s.label}
-                  </span>
                 </div>
               </Link>
-            </Card>
+              <div className="z-10 flex flex-col items-center">
+                <span className={`text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display`}>
+                  {s.handle}
+                </span>
+                <span className={`mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200`}>
+                  {s.label}
+                </span>
+              </div>
+            </div>
           ))}
         </div>
       </div>
