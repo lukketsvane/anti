b41d94a47e0dc9134f539f3a9e5396697c2b247d
unknown <iverfinne@gmail.com>
2023-09-03 14:54:39 +0200

    large

--- app/gallery/article.tsx
+++ app/gallery/article.tsx
@@ -1,7 +1,6 @@
 // ./app/gallery/article.tsx
 
 "use client";
-
 import React from 'react';
 import Image from 'next/image';
 
@@ -9,12 +8,14 @@ type GalleryArticleProps = {
   title: string;
   image: string;
   content: React.ReactNode;
+  index: number;
+  setFocusImage: (index: number | null) => void;
 };
 
-export const GalleryArticle: React.FC<GalleryArticleProps> = ({ title, image, content }) => {
+export const GalleryArticle: React.FC<GalleryArticleProps> = ({ title, image, content, index, setFocusImage }) => {
   return (
-    <article className="flex flex-col items-center w-full p-2">
-      <div className="relative w-full pb-full">
+    <article className="flex flex-col items-center w-full p-2" onClick={() => setFocusImage(index)}>
+      <div className="relative w-full pb-full hover:opacity-80 cursor-pointer">
         <div style={{ paddingTop: '100%' }}></div>
         <Image src={image} alt={title} objectFit="cover" layout="fill" className="absolute inset-0" />
       </div>
--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,23 +1,59 @@
 // ./app/gallery/page.tsx
-
-import React from 'react';
+"use client";
+import React, { useState, useEffect, KeyboardEvent } from 'react';
 import { allGalleryImages } from 'contentlayer/generated';
 import { GalleryArticle } from './article';
+import Image from 'next/image';
 
 const GalleryPage = () => {
+  const [focusImage, setFocusImage] = useState<number | null>(null);
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'ArrowRight' && focusImage !== null && focusImage < allGalleryImages.length - 1) {
+      setFocusImage(focusImage + 1);
+    }
+    if (event.key === 'ArrowLeft' && focusImage !== null && focusImage > 0) {
+      setFocusImage(focusImage - 1);
+    }
+    if (event.key === 'Escape') {
+      setFocusImage(null);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [focusImage]);
+
   return (
     <div className="flex flex-wrap items-center justify-center">
-      {allGalleryImages?.map((imageData) => (
+      {allGalleryImages?.map((imageData, index) => (
         <div key={imageData._id} className="w-full p-2 sm:w-1/2 md:w-1/4 lg:w-1/4">
           <GalleryArticle 
             title={imageData.title} 
             image={imageData.image} 
-            content={imageData.description} 
+            content={imageData.description}
+            index={index}
+            setFocusImage={setFocusImage}
           />
         </div>
       )) ?? 'No images found.'}
+
+{focusImage !== null && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center p-10 bg-black bg-opacity-70" onClick={() => setFocusImage(null)}>
+          <div className="relative w-full h-full">
+            <Image 
+              src={allGalleryImages[focusImage].image} 
+              alt={allGalleryImages[focusImage].title} 
+              objectFit="contain" 
+              layout="fill" 
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 
 export default GalleryPage;
