00fe7b91566120fa9597c4f3d914a9369165499d
unknown <iverfinne@gmail.com>
2023-09-06 00:04:46 +0200

    add constellation

--- app/components/constellation.tsx
+++ app/components/constellation.tsx
@@ -1,60 +1,79 @@
-// Constellation.tsx
-
 import React, { useEffect, useRef } from 'react';
-import Link from 'next/link';
-
-interface Star {
-  name: string;
-  x: number;
-  y: number;
-}
 
 const Constellation: React.FC = () => {
   const containerRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const stars: Star[] = [
-      { name: "Deneb", x: -6, y: 7 },
-      { name: "Sadr", x: -3, y: 3 },
-      { name: "Eta", x: 3, y: -2 },
-      { name: "Albireo", x: 7, y: -8 },
-      { name: "Gienha", x: -6, y: -2 },
-      { name: "Zeta", x: -10, y: -5 },
-      { name: "Fawaris", x: 4, y: 5 },
-      { name: "Iota", x: 6, y: 9 },
-      { name: "Cappa", x: 8, y: 10 },
+    const stars = [
+      { name: "Deneb", x: -6, y: 7, href: "/Deneb" },
+      { name: "Sadr", x: -3, y: 3, href: "/Sadr" },
+      { name: "Eta", x: 3, y: -2, href: "/Eta" },
+      { name: "Albireo", x: 7, y: -8, href: "/Albireo" },
+      { name: "Gienha", x: -6, y: -2, href: "/Gienha" },
+      { name: "Zeta", x: -10, y: -5, href: "/Zeta" },
+      { name: "Fawaris", x: 4, y: 5, href: "/Fawaris" },
+      { name: "Iota", x: 6, y: 9, href: "/Iota" },
+      { name: "Cappa", x: 8, y: 10, href: "/Cappa" }
     ];
 
     stars.forEach((star, i) => {
       const anchor = document.createElement('a');
       anchor.style.position = 'fixed';
-      anchor.style.left = `calc(50% + ${star.x * 20}px)`;
-      anchor.style.top = `calc(50% - ${star.y * 20}px)`;
-      anchor.style.width = '10px';  // Made smaller
-      anchor.style.height = '10px';  // Made smaller
-      anchor.style.backgroundColor = 'white';
-      anchor.style.borderRadius = '50%';
-      anchor.style.zIndex = '1';
-      anchor.href = `/${star.name.toLowerCase()}`;
+      anchor.style.left = `calc(40% + ${star.x * 20}px)`; // Adjusted horizontal position
+      anchor.style.top = `calc(30% - ${star.y * 20}px)`; // Adjusted vertical position
+      anchor.style.width = '10px';
+      anchor.style.height = '10px';
+      anchor.style.backgroundColor = 'transparent';
+      anchor.style.pointerEvents = 'auto';
+      anchor.style.zIndex = '11';
+      anchor.href = star.href;
       anchor.id = `star-${i}`;
 
-      const label = document.createElement('span');
-      label.textContent = star.name;
-      label.style.position = 'absolute';
-      label.style.bottom = '-20px';
-      label.style.left = '50%';
-      label.style.transform = 'translateX(-50%)';
-      label.style.fontSize = '12px';
-      label.style.color = 'white';
+      const dot = document.createElement('div');
+      dot.style.width = '2.5px';
+      dot.style.height = '2.5px';
+      dot.style.backgroundColor = 'white';
+      dot.style.borderRadius = '50%';
 
-      anchor.appendChild(label);
+      const nameSpan = document.createElement('span');
+      nameSpan.textContent = star.name;
+      nameSpan.style.color = 'white'; // Set text color to white
+      nameSpan.style.opacity = '0';
+      nameSpan.style.transition = 'opacity 0.5s ease';
+      nameSpan.style.position = 'absolute';
+      nameSpan.style.left = '50%';
+      nameSpan.style.bottom = '-20px';
+      nameSpan.style.transform = 'translate(-50%, 0)';
+      nameSpan.style.whiteSpace = 'nowrap';
+
+      anchor.appendChild(dot);
+      anchor.appendChild(nameSpan);
+
+      anchor.addEventListener('mouseenter', () => {
+        nameSpan.style.opacity = '1';
+      });
+
+      anchor.addEventListener('mouseleave', () => {
+        nameSpan.style.opacity = '0';
+      });
+
       containerRef.current?.appendChild(anchor);
     });
   }, []);
 
   return (
-    <div ref={containerRef} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: 10 }}>
-      {/* Other elements can go here if needed */}
+    <div 
+      ref={containerRef} 
+      style={{ 
+        position: 'fixed', 
+        top: '-40%', // Moved the component further up
+        left: '-10%', // Moved the component more to the left
+        width: '100%', 
+        height: '100%', 
+        zIndex: 10,
+        pointerEvents: 'none' 
+      }}
+    >
     </div>
   );
 };
