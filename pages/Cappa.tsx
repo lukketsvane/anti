4aeafafc70d2b3254defe985468994c782bb646c
unknown <iverfinne@gmail.com>
2023-09-06 02:48:55 +0200

    scoll issue mobile

--- pages/Cappa.tsx
+++ pages/Cappa.tsx
@@ -1,31 +1,23 @@
+// File: ./pages/Cappa.tsx
+
 import Head from 'next/head';
 import Link from 'next/link';
 import { ArrowLeft } from "lucide-react";
+import Shapes from '../app/components/shapes'; // Import the Shapes component
 
-const Deneb = () => {
-
-
-
+const Cappa = () => {
   return (
     <div className={`flex flex-col items-center justify-center w-screen h-screen overflow-hidden`}>
-
-      <Head>
-        <title>Deneb - T-Rex Runner Game</title>
-      </Head>
-
       <Link href="/">
+      <div className={`flex flex-col items-center justify-center w-screen h-screen overflow-hidden`}>
         <div style={{ position: 'fixed', top: '20px', left: '20px', zIndex: 50, cursor: 'pointer' }}>
+        </div>
           <ArrowLeft size={24} color="black" />
         </div>
       </Link>
-
-      <iframe 
-        src="/t-rex-runner/index.html" 
-        style={{ width: '100%', height: '92vh' }} 
-        frameBorder="0">
-      </iframe>
+      <Shapes /> {/* Use the Shapes component */}
     </div>
   );
 };
 
-export default Deneb;
+export default Cappa;
---
+++ pages/Gienha.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { ArrowLeft } from "lucide-react";
+
+const Gienha = () => {
+
+
+
+  return (
+    <div className={`flex flex-col items-center justify-center w-screen h-screen overflow-hidden`}>
+
+    </div>
+  );
+};
+
+export default Gienha;
