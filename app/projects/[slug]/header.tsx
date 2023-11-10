13363d413558f4a12e1c2011801339cca9e5ecd5
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-10 01:00:43 +0000

    ye

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -60,13 +60,15 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 
           <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
             <div className="grid grid-cols-1 gap-y-6 gap-x-8 text-base font-semibold leading-7 text-white sm:grid-cols-2 md:flex lg:gap-x-10">
-              {links.map((link) => (
-                <Link key={link.label} href={link.href}>
-                  <div target="_blank" className="hover:underline">
-                    {link.label} <span aria-hidden="true">&rarr;</span>
-                  </div>
-                </Link>
-              ))}
+              {
+                links.map((link) => (
+                  <Link key={link.label} href={link.href} passHref>
+                    <a target="_blank" className="hover:underline">
+                      {link.label} <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </Link>
+                ))
+              }
             </div>
           </div>
         </div>
