34850525ec94dd468dabcfe00b123f1aa8913261
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-14 02:46:35 +0000

    yes

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -37,7 +37,8 @@ export default async function PostPage({ params }: Props) {
 
 	return (
 		<div className="bg-zinc-50 min-h-screen">
-			<Header project={project} views={views} />
+			<Header project={project} views={views} darkMode={darkMode} />
+
 			<ReportView slug={project.slug} />
 
 			<article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
