079677e787daddf8198ea7165a21392e370977bc
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-14 02:40:32 +0000

    yes

--- app/projects/[slug]/view.tsx
+++ app/projects/[slug]/view.tsx
@@ -1,17 +1,37 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ReportView: React.FC<{ slug: string }> = ({ slug }) => {
-	useEffect(() => {
-		fetch("/api/incr", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ slug }),
-		});
-	}, [slug]);
+    const [isError, setError] = useState(false);
 
-	return null;
+    useEffect(() => {
+        const reportView = async () => {
+            try {
+                const response = await fetch("/api/incr", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ slug }),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Failed to report view');
+                }
+            } catch (error) {
+                console.error('Error reporting view:', error);
+                setError(true);
+            }
+        };
+
+        reportView();
+    }, [slug]);
+
+    // Optionally, you can render something based on the isError state
+    if (isError) {
+        return <div>Error in reporting view</div>;
+    }
+
+    return null;
 };
