c63228c27d86ad3c90c325dd458a2b1f2d020715
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-14 02:44:35 +0000

    yes

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -9,10 +9,11 @@ import { Redis } from "@upstash/redis";
 export const revalidate = 60;
 
 type Props = {
-    project: Project;
-    views?: number; // Made optional with '?'
-    darkMode?: boolean; // Made optional with '?'
+	params: {
+		slug: string;
+	};
 };
+
 const redis = Redis.fromEnv();
 
 export async function generateStaticParams(): Promise<Props["params"][]> {
@@ -30,13 +31,15 @@ export default async function PostPage({ params }: Props) {
 	if (!project) {
 		notFound();
 	}
-	const views = /* logic to determine views */;
-	const darkMode = /* logic to determine if dark mode is active */;
-	
+
+	const views =
+		(await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
+
 	return (
-		<div className="min-h-screen">
-			<Header project={project} views={views} darkMode={darkMode} />
+		<div className="bg-zinc-50 min-h-screen">
+			<Header project={project} views={views} />
 			<ReportView slug={project.slug} />
+
 			<article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
 				<Mdx code={project.body.code} />
 			</article>
