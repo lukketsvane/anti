6bebaf16d23bcf34c395122a2f3c07bc277203e3
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-22 22:44:07 +0000

    ye

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { appWithTranslation } from 'next-i18next';
-import nextI18NextConfig from '../next-i18next.config';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default appWithTranslation(MyApp, nextI18NextConfig);
