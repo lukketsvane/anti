ea0a2fa4e27263346d712d93cbd79509ed6d2573
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-11-29 08:08:21 +0000

    timeline

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
   },
   images: {
     domains: ['example.com'],
+  },
+  env: {
+    // Replace 'your-production-domain.com' with your actual domain for production
+    // During development, 'metadataBase' will fall back to 'http://localhost:3000'
+    metadataBase: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://www.iverfinne.no',
   }
 };
 
