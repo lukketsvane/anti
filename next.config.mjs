a01b7abb13d960751252f1c4cafcfd561d4c47fd
tastefinger <41840333+lukketsvane@users.noreply.github.com>
2023-12-16 12:49:16 +0000

    rm i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 import { withContentlayer } from 'next-contentlayer';
 
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'no'],
-    defaultLocale: 'en',
-    localeDetection: true,
-  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   experimental: {
     appDir: true,
